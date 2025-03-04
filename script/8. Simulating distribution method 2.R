94dc2d1ac1dead921a132f691900f091ce87015f
Lucas Brandao <lucasbrandao070@gmail.com>
2025-03-04 10:50:43 -0300

    trata valores vazios e renda ajustada infinita

--- script/8. Simulating distribution method 2.R
+++ script/8. Simulating distribution method 2.R
@@ -8,9 +8,17 @@ set.seed(123)  # For reproducibility
 
 # ========= 1. Extract Data and Compute Original Mean =========
 # Convert the survey design object to a data.frame and extract weights
-brazil_income_data  <- readRDS("./intermediarios/renda_PNADc_brasil2023visita_1.rds")
-df <- brazil_income_data$variables
-ordered_brazil_weights  <-  df  %>%  arrange(VD5008_DEF) %>% .$V1032
+brazil_data  <- readRDS("./intermediarios/renda_PNADc_brasil2023visita_1.rds")
+brazil_income_data <- subset(brazil_data, !is.na(VD5008_DEF))
+brazil_income_data  <- update(brazil_income_data,
+                              orig_order = 1:nrow(brazil_income_data$variables))
+
+brazil_income_data_df <- brazil_income_data$variables
+
+ordered_brazil_weights  <-  brazil_income_data_df  %>%
+                                arrange(VD5008_DEF) %>%
+                                .$V1032
+
 # Compute the original (weighted) mean income of Brazil
 mean_income <- as.numeric(svymean(~VD5008_DEF, brazil_income_data, na.rm = TRUE))
 
@@ -37,7 +45,7 @@ objective_function <- function(sigma) {
 
   # Após obter o mu, é criada uma amostra de 383.049 valores aleatórios que sigam a distribuição lognormal
   # com os parâmetros obtidos, e que replique os pesos da amostra do brasil, na ordem por renda.
-  sim_sample <- rlnorm(383049, meanlog = mu, sdlog = sigma)  %>% sort()
+  sim_sample <- rlnorm(382937, meanlog = mu, sdlog = sigma)  %>% sort()
   weight_sample  <- ordered_brazil_weights
   sample <- data.frame(sim_sample, weight_sample)
 
@@ -56,25 +64,35 @@ objective_function <- function(sigma) {
   # Theoretical cumulative decile shares (excluding 0%)
   theoretical_cum_shares <- cum_income_shares
 
-  # Compute error for decile shares
+  # depois de calcular a participação acumulada na renda teórica,
+  # o valor obtido é comparado com a distribuicao do paraguai
   error_shares <- sum((theoretical_cum_shares - uruguay_decile_shares)^2)
 
   # Theoretical Gini
+  # a partir dos valores teóricos obtidos calcula-se o gini teórico tambem
   theoretical_gini <- weighted.gini(x = sample$sim_sample, w = sample$weight_sample)$Gini
 
-  # Compute the error for the Gini
+  # e é computado o erro, comparando com o erro do uruguay
   error_gini <- (theoretical_gini - uruguai_gini_index)^2
 
   # Total loss function (weights can be adjusted if needed)
+  # calcula-se entao o erro compartilhado
   return(error_shares + error_gini)
 }
 
 # Optimize sigma
-initial_sigma <- sqrt(2) * qnorm((uruguai_gini_index + 1) / 2)
-opt_result <- optim(par = initial_sigma, fn = objective_function, method = "BFGS")
-sigma_target <- opt_result$par
-mu_target <- log(mean_income) - (sigma_target^2) / 2
 
+# calcula-se um sigma inicial com base no gini do uruguai
+initial_sigma <- sqrt(2) * qnorm((uruguai_gini_index + 1) / 2)
+
+# utiliza-se a funcao optim para encontrar o sigma que minimiza o erro da função acima
+opt_result <- optim(par = initial_sigma, fn = objective_function, method = "BFGS")
+
+# extrai o sigma do resultado
+sigma_target <- opt_result$par
+
+# calcula o mu a partir do sigma obtido
+mu_target <- log(mean_income) - (sigma_target^2) / 2
 
 # Define the target quantile function based on these parameters.
 target_quantile <- function(q) {
@@ -82,40 +100,51 @@ target_quantile <- function(q) {
 }
 
 # ========= 3. Quantile Mapping =========
-df <- df %>%
+brazil_income_data_df_c_renda_ajustada <- brazil_income_data_df %>%
   arrange(VD5008_DEF) %>%
   mutate(cum_weight = cumsum(V1032),
          total_weight = sum(V1032, na.rm = TRUE),
-         emp_quantile = cum_weight / total_weight)
+         emp_quantile = cum_weight / total_weight,
+         emp_quantile_adjusted = pmin(pmax(cum_weight / total_weight, 1e-7), 1 - 1e-7),
+         VD5008_DEF_adjusted = target_quantile(emp_quantile_adjusted))
 
-df <- df %>%
-  mutate(VD5008_DEF_adjusted = target_quantile(emp_quantile),
-         VD5008_DEF_adjusted = if_else(VD5008_DEF_adjusted == Inf, 46130.58, VD5008_DEF_adjusted))
+tail(brazil_income_data_df_c_renda_ajustada %>%
+        select(orig_order,cum_weight,
+              total_weight,
+              emp_quantile,
+              emp_quantile_adjusted,
+              VD5008_DEF,
+              VD5008_DEF_adjusted))
 
-# Adjust mean
-adjusted_mean <- sum(df$VD5008_DEF_adjusted * df$V1032, na.rm = TRUE) / sum(df$V1032, na.rm = TRUE)
-scaling_factor <- mean_income / adjusted_mean
-df <- df %>%
+renda_media  <- weighted.mean(brazil_income_data_df_c_renda_ajustada$VD5008_DEF,
+             brazil_income_data_df_c_renda_ajustada$V1032)
+
+renda_media_ajustada  <- weighted.mean(brazil_income_data_df_c_renda_ajustada$VD5008_DEF_adjusted,
+             brazil_income_data_df_c_renda_ajustada$V1032)
+
+scaling_factor <- renda_media / renda_media_ajustada
+brazil_income_data_df_c_renda_ajustada <- brazil_income_data_df_c_renda_ajustada %>%
   mutate(VD5008_DEF_adjusted = VD5008_DEF_adjusted * scaling_factor)
 
 # ========= 4. Update Survey Design =========
+# 4.1 atualizando a base com a ordem original
+# verifica-se abaixo que as bases estão em ordens diferentes:
+head(brazil_income_data_df %>% select(orig_order, VD5008_DEF))
+head(brazil_income_data_df_c_renda_ajustada %>% select(orig_order, VD5008_DEF,VD5008_DEF_adjusted))
+
+# Portanto, é necessário garantir que os novos valores de renda estejam atribuídos a observações corretas
+brazil_income_data_df_final <- brazil_income_data_df %>%
+  left_join(brazil_income_data_df_c_renda_ajustada %>%
+                select(orig_order, VD5008_DEF_adjusted),
+            by = "orig_order") %>%
+  arrange(orig_order)
+
+head(brazil_income_data_df_final%>% select(orig_order, VD5008_DEF,VD5008_DEF_adjusted))
+
 brazil_income_data_adjusted <- update(brazil_income_data,
-                                      VD5008_DEF_adjusted = df$VD5008_DEF_adjusted)
+                                      VD5008_DEF_adjusted = brazil_income_data_df_final$VD5008_DEF_adjusted)
+# ========= 5. Validação do resultado
 
-# ========= 5. Poverty Calculation =========
-poverty_line <- 1320 / 2
-brazil_income_data <- update(brazil_income_data,
-                             poor = as.numeric(VD5008_DEF < poverty_line))
-brazil_income_data_adjusted <- update(brazil_income_data_adjusted,
-                                      poor_adjusted = as.numeric(VD5008_DEF_adjusted < poverty_line))
+# ======= 7. saving result ======
 
-# Compute poverty rates
-original_poverty_rate <- as.numeric(svymean(~poor, brazil_income_data, na.rm = TRUE))
-adjusted_poverty_rate <- as.numeric(svymean(~poor_adjusted, brazil_income_data_adjusted, na.rm = TRUE))
-
-# ========= 6. Output =========
-cat("Optimal sigma:", sigma_target, "\n")
-cat("Optimal mu:", mu_target, "\n")
-cat("Poverty line:", poverty_line, "\n")
-cat("Original poverty rate:", original_poverty_rate, "\n")
-cat("Adjusted poverty rate:", adjusted_poverty_rate, "\n")
+brazil_income_data_adjusted %>% saveRDS("./intermediarios/renda_PNADc_brasil2023visita_1_ajustada_uruguai.rds")
